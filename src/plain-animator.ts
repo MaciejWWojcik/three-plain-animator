c9c50211a61e6e74bfb02c035f86e7663ce4676f
MaciejWWojcik <maciejwwojcik@gmail.com>
2019-04-19 23:19:46 +0200

    plain-animator (loop animations) added + singular animation handling in plain-singular-animator

---
+++ src/plain-animator.ts
@@ -0,0 +1,60 @@
+import { Observable, Subject } from 'rxjs';
+import { Clock, RepeatWrapping, Texture } from 'three';
+
+export class PlainAnimator {
+
+  protected currentFrameDisplayTime: number = 0;
+  protected currentFrame: number = 0;
+  protected clock: Clock = new Clock();
+  protected _end$: Subject<any> = new Subject<any>();
+  protected readonly frameDisplayDuration: number;
+
+  constructor(
+    public texture: Texture,
+    protected tilesAmountHorizontally: number,
+    protected tilesAmountVertically: number,
+    protected tilesTotalAmount: number,
+    framesPerSecond: number,
+  ) {
+    this.tilesTotalAmount -= 1; // indexing from 0
+    this.frameDisplayDuration = 1000 / framesPerSecond;
+    this.texture.wrapS = RepeatWrapping;
+    this.texture.wrapT = RepeatWrapping;
+    this.texture.repeat.set(1 / tilesAmountHorizontally, 1 / tilesAmountVertically);
+  }
+
+  public init(startFrame: number = 0): Texture {
+    this.currentFrame = startFrame;
+    this.currentFrameDisplayTime = 0;
+    this.clock = new Clock();
+    this.updateTexture();
+    return this.texture;
+  }
+
+  public animate(): void {
+    this.currentFrameDisplayTime += this.clock.getDelta() * 1000;
+
+    while (this.currentFrameDisplayTime > this.frameDisplayDuration) {
+      this.currentFrameDisplayTime -= this.frameDisplayDuration;
+      if (this.currentFrame === this.tilesTotalAmount) {
+        this._end$.next();
+      }
+      this.currentFrame = this.currentFrame < this.tilesTotalAmount ? this.currentFrame + 1 : 0;
+      this.updateTexture();
+    }
+  }
+
+  public get end$(): Observable<any> {
+    return this._end$.asObservable();
+  }
+
+  protected updateTexture() {
+    const tileHeight = 1 / this.tilesAmountVertically;
+
+    const currentColumn = this.currentFrame % this.tilesAmountHorizontally;
+    const currentRow = Math.floor(this.currentFrame / this.tilesAmountHorizontally);
+
+    this.texture.offset.x = currentColumn / this.tilesAmountHorizontally;
+    this.texture.offset.y = 1 - (currentRow / this.tilesAmountVertically) - tileHeight;
+  }
+}
---
+++ src/plain-singular-animator.ts
@@ -0,0 +1,38 @@
+import { PlainAnimator } from './plain-animator';
+import { Texture } from 'three';
+import { take } from 'rxjs/operators';
+
+export class PlainSingularAnimator extends PlainAnimator {
+
+  constructor(texture: Texture, tilesAmountHorizontally: number, tilesAmountVertically: number, tilesTotalAmount: number, framesPerSecond: number) {
+    super(texture, tilesAmountHorizontally, tilesAmountVertically, tilesTotalAmount, framesPerSecond);
+  }
+
+  public play() {
+    let requestId: number;
+    const animation = () => {
+      this.animate();
+      requestId = requestAnimationFrame(animation);
+    };
+    requestId = requestAnimationFrame(animation);
+    this.end$.pipe(take(1)).toPromise().then(() => cancelAnimationFrame(requestId));
+  }
+
+  public animate(timestamp?: number): void {
+    this.currentFrameDisplayTime += this.clock.getDelta() * 1000;
+
+    while (this.currentFrameDisplayTime > this.frameDisplayDuration) {
+      this.currentFrameDisplayTime -= this.frameDisplayDuration;
+
+      if (this.currentFrame < this.tilesTotalAmount) {
+        this.currentFrame += 1;
+      }
+      this.updateTexture();
+      if (this.currentFrame === this.tilesTotalAmount) {
+        this._end$.next(true);
+        return;
+      }
+    }
+  }
+
+}

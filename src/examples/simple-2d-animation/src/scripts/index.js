2f4339c263eefbbcff6b99bab3c1cc1c9dd1e39d
Maciej Wojcik <maciejwwojcik@gmail.com>
2022-04-09 17:47:16 +0200

    update dependencies

--- src/examples/simple-2d-animation/src/scripts/index.js
+++ src/examples/simple-2d-animation/src/scripts/index.js
@@ -1,45 +1,38 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-var three_1 = require('three');
-var three_plain_animator_1 = require('three-plain-animator');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var three_1 = require("three");
+var three_plain_animator_1 = require("three-plain-animator");
 var camera;
 var renderer;
 var animator;
 function initScene() {
-  var frustumSize = 1000;
-  var scene = new three_1.Scene();
-  var aspect = window.innerWidth / window.innerHeight;
-  camera = new three_1.OrthographicCamera(
-    (frustumSize * aspect) / -2,
-    (frustumSize * aspect) / 2,
-    frustumSize / 2,
-    frustumSize / -2,
-    1,
-    2000,
-  );
-  renderer = new three_1.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
-  scene.background = new three_1.Color('#1976D2');
-  camera.position.z = 10;
-  return scene;
+    var frustumSize = 1000;
+    var scene = new three_1.Scene();
+    var aspect = window.innerWidth / window.innerHeight;
+    camera = new three_1.OrthographicCamera((frustumSize * aspect) / -2, (frustumSize * aspect) / 2, frustumSize / 2, frustumSize / -2, 1, 2000);
+    renderer = new three_1.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+    scene.background = new three_1.Color('#1976D2');
+    camera.position.z = 10;
+    return scene;
 }
 function animate(timestamp) {
-  renderer.render(scene, camera);
-  animator.animate();
-  requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+    animator.animate();
+    requestAnimationFrame(animate);
 }
 function createAnimatedMesh() {
-  var texturePath = 'textures/texture.png';
-  /* @ts-ignore due to type errors from using PlanAnimator from local files, not from node_modules */
-  animator = new three_plain_animator_1.PlainAnimator(new three_1.TextureLoader().load(texturePath), 4, 4, 10, 10);
-  var geometry = new three_1.PlaneGeometry(512, 512);
-  var texture = animator.init();
-  /* @ts-ignore due to type errors from using PlanAnimator from local files, not from node_modules */
-  var material = new three_1.MeshBasicMaterial({ map: texture, transparent: true });
-  var mesh = new three_1.Mesh(geometry, material);
-  mesh.position.set(0, 0, 1);
-  return mesh;
+    var texturePath = 'textures/texture.png';
+    /* @ts-ignore due to type errors from using PlanAnimator from local files, not from node_modules */
+    animator = new three_plain_animator_1.PlainAnimator(new three_1.TextureLoader().load(texturePath), 4, 4, 10, 10);
+    var geometry = new three_1.PlaneGeometry(512, 512);
+    var texture = animator.init();
+    /* @ts-ignore due to type errors from using PlanAnimator from local files, not from node_modules */
+    var material = new three_1.MeshBasicMaterial({ map: texture, transparent: true });
+    var mesh = new three_1.Mesh(geometry, material);
+    mesh.position.set(0, 0, 1);
+    return mesh;
 }
 var scene = initScene();
 var mesh = createAnimatedMesh();
